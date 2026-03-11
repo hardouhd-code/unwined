4f22fc6e52c098a8103d55e9a39da668509fe96e
hardouhd-code <hardou.hd@gmail.com>
2026-03-11 17:06:51 +0100

    Delete src directory

--- src/lib/helpers.js
+++
@@ -1,7 +0,0 @@
-import { WINE_TYPES } from "./constants";
-
-export const haptic = (p=50) => { try { if(navigator.vibrate) navigator.vibrate(p); } catch(_) {} };
-export const typeColor = (id) => WINE_TYPES.find(t=>t.id===id)?.color || "#B8862A";
-export const typeLight = (id) => WINE_TYPES.find(t=>t.id===id)?.light || "rgba(184,134,42,.1)";
-export const typeEmoji = (id) => WINE_TYPES.find(t=>t.id===id)?.emoji || "🍷";
-
