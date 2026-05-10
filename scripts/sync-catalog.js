c425a313711e65d54e12dfc035cb4e1ec1fbdf39
hardouhd-code <hardou.hd@gmail.com>
2026-05-10 16:37:29 +0200

    Update sync-catalog.js

--- scripts/sync-catalog.js
+++ scripts/sync-catalog.js
@@ -29,40 +29,113 @@ const WINE_PRODUCT_TYPES = new Set([
   'zoete wijn', 'versterkte wijn', 'oranje wijn', 'all', 'bio'
 ]);
 
+const KNOWN_WINE_VENDORS = [
+  // Afrique du Sud
+  'kanonkop', 'meerlust', 'boschendal', 'stellenrust', 'warwick', 'rustenberg',
+  'spier', 'waterford', 'klein constantia', 'hamilton russell', 'bouchard finlayson',
+  'mullineux', 'leeu', 'sadie', 'boekenhoutskloof', 'creation wines', 'ataraxia',
+  'delheim', 'grangehurst', 'jordan', 'neil ellis', 'simonsig', 'thelema',
+  // Nouvelle-Zélande
+  'cloudy bay', 'villa maria', 'saint clair', 'greywacke', 'dog point',
+  'seresin', 'craggy range', 'te mata', 'felton road',
+  // Australie
+  'penfolds', 'henschke', 'leeuwin', 'grosset', 'jim barry', 'd arenberg',
+  'torbreck', 'two hands', 'yalumba', 'peter lehmann', 'wolf blass', 'jacob s creek',
+  'shaw smith', 'mount langhi', 'jasper hill', 'bass phillip',
+  // Argentine
+  'catena', 'achaval ferrer', 'clos de los siete', 'zuccardi', 'trapiche',
+  'norton', 'luigi bosca', 'alta vista', 'clos de chacras',
+  // Chili
+  'concha y toro', 'almaviva', 'montes', 'errazuriz', 'vina maipo',
+  'casa lapostolle', 'ventisquero', 'emiliana',
+  // Georgie
+  'pheasant s tears', 'orgo', 'gotsa', 'iago', 'jakeli', 'teliani valley',
+];
+
 const COUNTRY_RULES = [
-  { country: 'France', keys: ['france', 'bordeaux', 'bourgogne', 'rhone', 'loire', 'alsace', 'champagne', 'provence', 'languedoc'] },
-  { country: 'Italie', keys: ['italie', 'italia', 'toscane', 'toscana', 'piemont', 'piemonte', 'veneto', 'venetie', 'sicile', 'sicilia', 'puglia', 'barolo', 'chianti', 'brunello'] },
-  { country: 'Espagne', keys: ['espagne', 'espana', 'rioja', 'priorat', 'ribera del duero', 'jerez', 'cava', 'rueda', 'ribeiro'] },
-  { country: 'Portugal', keys: ['portugal', 'douro', 'dao', 'alentejo', 'vinho verde', 'bairrada'] },
-  { country: 'Argentine', keys: ['argentine', 'argentina', 'mendoza', 'salta', 'patagonie', 'malbec'] },
-  { country: 'Chili', keys: ['chili', 'chile', 'maipo', 'colchagua', 'casablanca valley'] },
-  { country: 'Allemagne', keys: ['allemagne', 'germany', 'mosel', 'moselle', 'rheingau', 'pfalz'] },
-  { country: 'Afrique du Sud', keys: ['afrique du sud', 'south africa', 'stellenbosch', 'paarl', 'franschhoek'] },
-  { country: 'États-Unis', keys: ['usa', 'united states', 'etats-unis', 'napa', 'sonoma', 'oregon'] },
-  { country: 'Nouvelle-Zélande', keys: ['new zealand', 'nouvelle-zelande', 'marlborough', 'central otago'] },
-  { country: 'Belgique', keys: ['belgique', 'belgie'] }
+  { country: 'France', keys: ['france', 'bordeaux', 'bourgogne', 'rhone', 'loire', 'alsace', 'champagne', 'provence', 'languedoc', 'roussillon', 'sud-ouest', 'jura', 'savoie'] },
+  { country: 'Italie', keys: ['italie', 'italia', 'toscane', 'toscana', 'piemont', 'piemonte', 'veneto', 'venetie', 'sicile', 'sicilia', 'puglia', 'barolo', 'chianti', 'brunello', 'amarone', 'prosecco', 'soave', 'gavi'] },
+  { country: 'Afrique du Sud', keys: [
+    'afrique du sud', 'south africa', 'stellenbosch', 'paarl', 'franschhoek',
+    'swartland', 'constantia', 'walker bay', 'elgin', 'robertson', 'hemel en aarde',
+    'kanonkop', 'meerlust', 'boschendal', 'stellenrust', 'warwick', 'rustenberg',
+    'spier', 'waterford', 'klein constantia', 'hamilton russell', 'bouchard finlayson',
+    'mullineux', 'boekenhoutskloof', 'delheim', 'jordan', 'neil ellis', 'simonsig',
+    'thelema', 'pinotage', 'cape blend', 'western cape'
+  ]},
+  { country: 'Espagne', keys: ['espagne', 'espana', 'rioja', 'priorat', 'ribera del duero', 'jerez', 'cava', 'rueda', 'ribeiro', 'rias baixas', 'penedes', 'navarra'] },
+  { country: 'Belgique', keys: ['belgique', 'belgie', 'belgium'] },
+  { country: 'Portugal', keys: ['portugal', 'douro', 'dao', 'alentejo', 'vinho verde', 'bairrada', 'setubal', 'madeira', 'porto'] },
+  { country: 'Argentine', keys: ['argentine', 'argentina', 'mendoza', 'salta', 'patagonie', 'malbec argentin', 'catena', 'achaval', 'zuccardi', 'trapiche', 'norton', 'luigi bosca'] },
+  { country: 'Australie', keys: [
+    'australie', 'australia', 'barossa', 'clare valley', 'eden valley', 'mclaren vale',
+    'margaret river', 'hunter valley', 'yarra valley', 'coonawarra', 'adelaide hills',
+    'penfolds', 'henschke', 'leeuwin', 'grosset', 'jim barry', 'd arenberg',
+    'torbreck', 'two hands', 'yalumba', 'peter lehmann', 'wolf blass', 'shaw smith',
+    'shiraz australia', 'cabernet australia'
+  ]},
+  { country: 'Nouvelle-Zélande', keys: [
+    'new zealand', 'nouvelle-zelande', 'marlborough', 'central otago',
+    'hawkes bay', 'waipara', 'cloudy bay', 'villa maria', 'saint clair',
+    'greywacke', 'dog point', 'seresin', 'craggy range', 'te mata', 'felton road'
+  ]},
+  { country: 'Autriche', keys: ['autriche', 'austria', 'gruner veltliner', 'gruner', 'weinviertel', 'wachau', 'kamptal', 'burgenland', 'blaufrankisch', 'zweigelt'] },
+  { country: 'Chili', keys: ['chili', 'chile', 'maipo', 'colchagua', 'casablanca valley', 'concha y toro', 'almaviva', 'montes', 'errazuriz', 'casa lapostolle'] },
+  { country: 'Allemagne', keys: ['allemagne', 'germany', 'mosel', 'moselle', 'rheingau', 'pfalz', 'nahe', 'ahr', 'franken', 'spatlese', 'auslese'] },
+  { country: 'États-Unis', keys: ['usa', 'united states', 'etats-unis', 'napa', 'sonoma', 'oregon', 'washington state', 'willamette', 'paso robles'] },
+  { country: 'Georgie', keys: [
+    'georgie', 'georgia', 'georgian wine', 'kakheti', 'kartli', 'imereti', 'racha',
+    'rkatsiteli', 'saperavi', 'qvevri', 'kvevri', 'amber wine georgia',
+    'pheasant s tears', 'orgo', 'gotsa', 'iago', 'jakeli', 'teliani'
+  ]},
+  { country: 'Grèce', keys: ['grece', 'greece', 'assyrtiko', 'santorini', 'nemea', 'xinomavro', 'moschofilero', 'naoussa', 'crete'] },
+  { country: 'Luxembourg', keys: ['luxembourg', 'luxemburg', 'moselle luxembourgeoise', 'remich', 'grevenmacher', 'auxerrois luxembourg', 'rivaner'] },
 ];
 
 const REGION_RULES = [
-  { region: 'Bordeaux', keys: ['bordeaux', 'margaux', 'pauillac', 'saint emilion', 'st emilion', 'pomerol', 'medoc', 'haut medoc', 'sauternes', 'pessac leognan', 'graves'] },
-  { region: 'Bourgogne', keys: ['bourgogne', 'chablis', 'beaune', 'gevrey', 'chambertin', 'meursault', 'montrachet', 'nuits saint georges', 'pommard', 'volnay', 'macon'] },
-  { region: 'Rhône', keys: ['rhone', 'cote rotie', 'hermitage', 'crozes hermitage', 'condrieu', 'chateauneuf du pape', 'gigondas', 'vacqueyras', 'lirac', 'ventoux'] },
-  { region: 'Loire', keys: ['loire', 'sancerre', 'pouilly fum', 'chinon', 'saumur', 'vouvray', 'muscadet', 'anjou'] },
-  { region: 'Alsace', keys: ['alsace', 'riesling', 'gewurztraminer', 'pinot gris alsace'] },
-  { region: 'Champagne', keys: ['champagne', 'blanc de blancs', 'blanc de noirs'] },
-  { region: 'Languedoc-Roussillon', keys: ['languedoc', 'roussillon', 'corbieres', 'minervois', 'fitou', 'faugeres', 'pic saint loup'] },
-  { region: 'Provence', keys: ['provence', 'bandol', 'cassis', 'coteaux d aix'] },
-  { region: 'Beaujolais', keys: ['beaujolais', 'fleurie', 'morgon', 'brouilly', 'moulin a vent', 'juli', 'regnie', 'saint amour'] },
-  { region: 'Piémont', keys: ['piemont', 'piemonte', 'barolo', 'barbaresco', 'gavi'] },
-  { region: 'Toscane', keys: ['toscane', 'toscana', 'chianti', 'brunello', 'montalcino', 'bolgheri', 'vino nobile di montepulciano'] },
-  { region: 'Vénétie', keys: ['venetie', 'veneto', 'amarone', 'valpolicella', 'soave', 'prosecco'] },
-  { region: 'Sicile', keys: ['sicile', 'sicilia', 'etna', 'marsala'] },
+  { region: 'Bordeaux', keys: ['bordeaux', 'margaux', 'pauillac', 'saint emilion', 'st emilion', 'pomerol', 'medoc', 'haut medoc', 'sauternes', 'pessac leognan', 'graves', 'barsac', 'fronsac'] },
+  { region: 'Bourgogne', keys: ['bourgogne', 'chablis', 'beaune', 'gevrey', 'chambertin', 'meursault', 'montrachet', 'nuits saint georges', 'pommard', 'volnay', 'macon', 'givry', 'rully', 'mercurey'] },
+  { region: 'Rhône', keys: ['rhone', 'cote rotie', 'hermitage', 'crozes hermitage', 'condrieu', 'chateauneuf du pape', 'gigondas', 'vacqueyras', 'lirac', 'ventoux', 'luberon', 'costières de nimes'] },
+  { region: 'Loire', keys: ['loire', 'sancerre', 'pouilly fum', 'chinon', 'saumur', 'vouvray', 'muscadet', 'anjou', 'bourgueil', 'touraine', 'savennieres'] },
+  { region: 'Alsace', keys: ['alsace', 'gewurztraminer', 'pinot gris alsace', 'alsace grand cru'] },
+  { region: 'Champagne', keys: ['champagne', 'blanc de blancs', 'blanc de noirs', 'brut nature', 'extra brut'] },
+  { region: 'Languedoc-Roussillon', keys: ['languedoc', 'roussillon', 'corbieres', 'minervois', 'fitou', 'faugeres', 'pic saint loup', 'saint chinian', 'picpoul', 'muscat de rivesaltes'] },
+  { region: 'Provence', keys: ['provence', 'bandol', 'cassis', 'coteaux d aix', 'les baux', 'palette'] },
+  { region: 'Beaujolais', keys: ['beaujolais', 'fleurie', 'morgon', 'brouilly', 'moulin a vent', 'julienas', 'regnie', 'saint amour', 'chiroubles', 'chenas'] },
+  { region: 'Piémont', keys: ['piemont', 'piemonte', 'barolo', 'barbaresco', 'gavi', 'barbera', 'dolcetto', 'moscato d asti', 'asti'] },
+  { region: 'Toscane', keys: ['toscane', 'toscana', 'chianti', 'brunello', 'montalcino', 'bolgheri', 'vino nobile di montepulciano', 'morellino', 'vernaccia'] },
+  { region: 'Vénétie', keys: ['venetie', 'veneto', 'amarone', 'valpolicella', 'soave', 'prosecco', 'bardolino', 'pinot grigio veneto'] },
+  { region: 'Sicile', keys: ['sicile', 'sicilia', 'etna', 'marsala', 'nero d avola', 'grillo', 'catarratto'] },
   { region: 'Rioja', keys: ['rioja'] },
   { region: 'Ribera del Duero', keys: ['ribera del duero'] },
   { region: 'Priorat', keys: ['priorat'] },
-  { region: 'Douro', keys: ['douro'] },
+  { region: 'Douro', keys: ['douro', 'porto', 'port'] },
   { region: 'Alentejo', keys: ['alentejo'] },
-  { region: 'Mendoza', keys: ['mendoza'] }
+  { region: 'Mendoza', keys: ['mendoza', 'lujan de cuyo'] },
+  // Afrique du Sud
+  { region: 'Stellenbosch', keys: ['stellenbosch'] },
+  { region: 'Franschhoek', keys: ['franschhoek'] },
+  { region: 'Paarl', keys: ['paarl'] },
+  { region: 'Swartland', keys: ['swartland'] },
+  { region: 'Walker Bay', keys: ['walker bay', 'hemel en aarde'] },
+  { region: 'Constantia', keys: ['constantia', 'klein constantia'] },
+  { region: 'Elgin', keys: ['elgin south africa'] },
+  // Australie
+  { region: 'Barossa Valley', keys: ['barossa'] },
+  { region: 'Clare Valley', keys: ['clare valley'] },
+  { region: 'McLaren Vale', keys: ['mclaren vale'] },
+  { region: 'Margaret River', keys: ['margaret river'] },
+  { region: 'Yarra Valley', keys: ['yarra valley'] },
+  { region: 'Adelaide Hills', keys: ['adelaide hills'] },
+  // Nouvelle-Zélande
+  { region: 'Marlborough', keys: ['marlborough'] },
+  { region: 'Central Otago', keys: ['central otago'] },
+  { region: "Hawke's Bay", keys: ['hawkes bay'] },
+  // Georgie
+  { region: 'Kakheti', keys: ['kakheti'] },
+  { region: 'Imereti', keys: ['imereti'] },
+  // Luxembourg
+  { region: 'Moselle Luxembourgeoise', keys: ['moselle luxembourgeoise', 'luxembourg moselle'] },
 ];
 
 const AOP_RULES = [
@@ -72,7 +145,7 @@ const AOP_RULES = [
   'sancerre', 'chinon', 'vouvray', 'muscadet', 'champagne', 'cremant de bourgogne',
   'alsace grand cru', 'barolo', 'barbaresco', 'chianti classico', 'brunello di montalcino',
   'amarone della valpolicella', 'valpolicella', 'prosecco', 'rioja', 'ribera del duero',
-  'priorat', 'douro', 'dao', 'vinho verde', 'mendoza'
+  'priorat', 'douro', 'dao', 'vinho verde', 'mendoza', 'stellenbosch wo', 'western cape wo'
 ];
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
@@ -92,10 +165,10 @@ function matchFirst(ruleSet, haystack, fallback = 'Autre') {
 }
 
 function detectType(haystack) {
-  if (/(champagne|cremant|prosecco|cava|mousseux|effervescent|pet nat|petillant|schuimwijn|sparkling)/.test(haystack)) return 'Effervescent';
-  if (/(rose|rosado|rosato|rose wijn)/.test(haystack)) return 'Rosé';
-  if (/(blanc|white|bianco|witte wijn|vin blanc)/.test(haystack)) return 'Blanc';
-  if (/(rouge|red|rosso|rode wijn|vin rouge)/.test(haystack)) return 'Rouge';
+  if (/(champagne|cremant|prosecco|cava|mousseux|effervescent|pet nat|petillant|schuimwijn|sparkling|cap classique)/.test(haystack)) return 'Effervescent';
+  if (/(rose|rosado|rosato|rose wijn|blush)/.test(haystack)) return 'Rosé';
+  if (/(blanc|white|bianco|witte wijn|vin blanc|chenin|sauvignon blanc|chardonnay|riesling|viognier|gruner|pinot gris|gewurz|albarino|vermentino|rkatsiteli|assyrtiko|auxerrois|rivaner|gruner veltliner)/.test(haystack)) return 'Blanc';
+  if (/(rouge|red|rosso|rode wijn|vin rouge|pinotage|cabernet|syrah|shiraz|merlot|malbec|pinot noir|nebbiolo|sangiovese|tempranillo|grenache|mourvedre|saperavi|xinomavro|blaufrankisch|zweigelt)/.test(haystack)) return 'Rouge';
   return 'Autre';
 }
 
@@ -103,7 +176,7 @@ function detectAop(haystack) {
   for (const term of AOP_RULES) {
     if (haystack.includes(normalize(term))) return term.toUpperCase();
   }
-  const m = haystack.match(/\b([a-z]{3,}(?:\s+[a-z]{3,}){0,3})\s+(aop|aoc|docg|doc|igp)\b/i);
+  const m = haystack.match(/\b([a-z]{3,}(?:\s+[a-z]{3,}){0,3})\s+(aop|aoc|docg|doc|igp|wo)\b/i);
   if (m) return `${m[1]} ${m[2]}`.replace(/\s+/g, ' ').toUpperCase();
   return 'N/A';
 }
@@ -127,7 +200,7 @@ function isWine(product) {
   const productType = normalize(product.product_type || '');
   const tags = normalize((Array.isArray(product.tags) ? product.tags.join(' ') : product.tags) || '');
   const vendor = normalize(product.vendor || '');
-  const bag = `${title} ${productType} ${tags}`;
+  const bag = `${title} ${productType} ${tags} ${vendor}`;
 
   if (EXCLUDE.some((w) => bag.includes(normalize(w)))) return false;
   if (/(non food|bier|beer|aperitif|spirits|spiritueux|tools|accessoires|accessory)/.test(productType)) return false;
@@ -135,12 +208,18 @@ function isWine(product) {
   if (/(box|boir-box|themabox|inspiratiebox|degustatiebox|coffret)/.test(title)) return false;
 
   const hasNonWineHints = NON_WINE_HINTS.some((w) => bag.includes(normalize(w)));
+  if (hasNonWineHints) return false;
+
   if (WINE_PRODUCT_TYPES.has(productType)) return true;
   if (/(wijn|wine|vin|rode wijn|witte wijn|rose wijn|schuimwijn|versterkte wijn|zoete wijn|oranje wijn)/.test(productType)) return true;
 
-  const looksLikeWine = /(vin|wine|rouge|blanc|rose|champagne|cremant|barolo|chianti|bordeaux|bourgogne|rhone|rioja|douro|prosecco|pinot|cabernet|syrah|riesling|sauvignon|nebbiolo|sangiovese)/.test(bag);
-  const hasWineVendor = /(domaine|chateau|winery|wines|vineyards|vigneron)/.test(vendor);
-  return (looksLikeWine && !hasNonWineHints) || (hasWineVendor && !hasNonWineHints);
+  const isKnownWineVendor = KNOWN_WINE_VENDORS.some(v => vendor.includes(normalize(v)));
+  if (isKnownWineVendor) return true;
+
+  const looksLikeWine = /(vin|wine|rouge|blanc|rose|champagne|cremant|barolo|chianti|bordeaux|bourgogne|rhone|rioja|douro|prosecco|pinot|cabernet|syrah|shiraz|riesling|sauvignon|nebbiolo|sangiovese|malbec|pinotage|chenin|viognier|chardonnay|tempranillo|grenache|mourvedre|zinfandel|cap classique|stellenbosch|marlborough|mendoza|barossa|saperavi|rkatsiteli|assyrtiko|gruner|blaufrankisch|zweigelt|auxerrois|rivaner)/.test(bag);
+  const hasWineVendor = /(domaine|chateau|winery|wines|vineyards|vigneron|estate|cellars|vignerons|cave|mas|clos|bodega|cantina|weingut|quinta)/.test(vendor);
+
+  return looksLikeWine || hasWineVendor;
 }
 
 async function fetchAllProducts() {
@@ -192,7 +271,7 @@ function buildReport(catalog) {
     if (wine.a && wine.a !== 'N/A') withAop++;
   }
   const top = (obj, n = 10) => Object.entries(obj).sort((a, b) => b[1] - a[1]).slice(0, n);
-  return { total: catalog.length, withAop, byCountry: top(byCountry, 12), byRegion: top(byRegion, 12), byType: top(byType, 8) };
+  return { total: catalog.length, withAop, byCountry: top(byCountry, 16), byRegion: top(byRegion, 15), byType: top(byType, 10) };
 }
 
 async function update() {
@@ -254,7 +333,6 @@ export function getRandomWines(n = 3) {
     .map((w) => ({ ...w, title: w.t, price: w.p, vendor: w.v, url: w.u, image: w.img, region: w.r, country: w.c, aop: w.a, type: w.y }));
 }`;
 
-  // Ne pas écrire si le nombre de vins n'a pas changé
   let existingContent = '';
   try { existingContent = fs.readFileSync(CATALOG_PATH, 'utf8'); } catch { /* premier run */ }
   const existingCount = (existingContent.match(/"t":/g) || []).length;
